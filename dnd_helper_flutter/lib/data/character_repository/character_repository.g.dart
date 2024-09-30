84dbf9d61b6c2c5295f0f3d46c44148e136864d0
EliteNoobest <Valvekat@mail.ru>
2024-09-30 12:46:51 +0300

    Add character_repository placeholder

---
+++ dnd_helper_flutter/lib/data/character_repository/character_repository.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'character_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$characterRepositoryHash() =>
+    r'7139896165e8c8b189cbb9056d5f051e12b3edbd';
+
+/// See also [CharacterRepository].
+@ProviderFor(CharacterRepository)
+final characterRepositoryProvider =
+    AsyncNotifierProvider<CharacterRepository, List<CharacterData>?>.internal(
+  CharacterRepository.new,
+  name: r'characterRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$characterRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CharacterRepository = AsyncNotifier<List<CharacterData>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
