057e1a34697efc3258ac2657614323a978ee128a
Lezhev <lezhevmatvey@ya.ru>
2024-10-19 08:42:23 +0300

    attributes bookmark added

--- dnd_helper_flutter/lib/app/character_sheet/character_sheet.dart
+++ dnd_helper_flutter/lib/app/character_sheet/character_sheet.dart
@@ -4,7 +4,9 @@ import 'package:dnd_helper_flutter/app/character_sheet/pages/fight.dart';
 import 'package:dnd_helper_flutter/app/character_sheet/pages/inventory.dart';
 import 'package:dnd_helper_flutter/app/character_sheet/pages/personality.dart';
 import 'package:dnd_helper_flutter/app/character_sheet/pages/spells.dart';
+import 'package:dnd_helper_flutter/app/character_sheet/widgets/attributes_tab_bar_view.dart';
 import 'package:dnd_helper_flutter/app/character_sheet/widgets/sheet_header.dart';
+import 'package:dnd_helper_flutter/app/character_sheet/widgets/widgets_state/widgets_state.dart';
 import 'package:dnd_helper_flutter/domain/build_context_extension.dart';
 import 'package:dnd_helper_flutter/ui/basic_widgets.dart';
 import 'package:flutter/material.dart';
@@ -17,98 +19,97 @@ class CharacterSheet extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final characterState = ref.watch(characterSheetStateProvider);
+    final widgetsState = ref.watch(widgetsStateProvider);
     return characterState.when(
       data: (characterState) {
         return LayoutBuilder(
           builder: (context, constraints) {
-            if (context.isMobile) {
-              return OrientationBuilder(
-                builder: (context, constrains) {
-                  return Scaffold(
+            return OrientationBuilder(
+              builder: (context, constrains) {
+                return DefaultTabController(
+                  length: 5,
+                  initialIndex: 0,
+                  child: Scaffold(
                     appBar: AppBar(
                       title: SheetHeader(
                           characterData: characterState.characterData),
                       centerTitle: true,
+                      toolbarHeight: 300,
+                      bottom: context.isMobile
+                          ? null
+                          : TabBar(
+                              onTap: (index) {
+                                ref
+                                    .read(widgetsStateProvider.notifier)
+                                    .onTabBarTap(index);
+                              },
+                              tabAlignment: TabAlignment.center,
+                              tabs: const <Tab>[
+                                Tab(
+                                  text: 'Бой',
+                                ),
+                                Tab(
+                                  text: 'Умения',
+                                ),
+                                Tab(
+                                  text: 'Инвентарь',
+                                ),
+                                Tab(
+                                  text: 'Личность',
+                                ),
+                                Tab(
+                                  text: 'Заклинания',
+                                ),
+                              ],
+                            ),
                     ),
-                    body: ref
-                        .read(characterSheetStateProvider.notifier)
-                        .getPage(characterState.selectedIndex),
-                    bottomNavigationBar: BottomNavigationBar(
-                      useLegacyColorScheme: false,
-                      onTap: (index) {
-                        ref
-                            .read(characterSheetStateProvider.notifier)
-                            .onTabBarTap(index);
-                      },
-                      currentIndex: characterState.selectedIndex,
-                      items: const <BottomNavigationBarItem>[
-                        BottomNavigationBarItem(
-                            icon: Icon(Icons.access_alarm_outlined),
-                            label: 'бой'),
-                        BottomNavigationBarItem(
-                            icon: Icon(Icons.access_alarm_outlined),
-                            label: 'способности'),
-                        BottomNavigationBarItem(
-                            icon: Icon(Icons.access_alarm_outlined),
-                            label: 'инвентарь'),
-                        BottomNavigationBarItem(
-                            icon: Icon(Icons.access_alarm_outlined),
-                            label: 'личность'),
-                        BottomNavigationBarItem(
-                            icon: Icon(Icons.access_alarm_outlined),
-                            label: 'заклинания'),
-                      ],
-                    ),
-                  );
-                },
-              );
-            } else {
-              return DefaultTabController(
-                length: 5,
-                initialIndex: characterState.selectedIndex,
-                child: Scaffold(
-                  appBar: AppBar(
-                    title: SheetHeader(
-                        characterData: characterState.characterData),
-                    bottom: TabBar(
-                      onTap: (index) {
-                        ref
-                            .read(characterSheetStateProvider.notifier)
-                            .onTabBarTap(index);
-                      },
-                      tabAlignment: TabAlignment.center,
-                      tabs: const [
-                        Tab(
-                          text: 'Бой',
-                        ),
-                        Tab(
-                          text: 'Умения',
-                        ),
-                        Tab(
-                          text: 'Инвентарь',
-                        ),
-                        Tab(
-                          text: 'Личность',
-                        ),
-                        Tab(
-                          text: 'Заклинания',
-                        ),
-                      ],
-                    ),
-                    centerTitle: true,
+                    body: context.isMobile
+                        ? ref
+                            .read(widgetsStateProvider.notifier)
+                            .getPage(widgetsState.selectedPage)
+                        : const Expanded(
+                            child: TabBarView(
+                              children: <Widget>[
+                                Fight(),
+                                Abilities(),
+                                Inventory(),
+                                Personality(),
+                                Spells(),
+                              ],
+                            ),
+                          ),
+                    bottomNavigationBar: context.isMobile
+                        ? BottomNavigationBar(
+                            useLegacyColorScheme: false,
+                            onTap: (index) {
+                              ref
+                                  .read(widgetsStateProvider.notifier)
+                                  .onTabBarTap(index);
+                            },
+                            currentIndex: widgetsState.selectedPage,
+                            items: const <BottomNavigationBarItem>[
+                              BottomNavigationBarItem(
+                                  icon: Icon(Icons.access_alarm_outlined),
+                                  label: 'бой'),
+                              BottomNavigationBarItem(
+                                  icon: Icon(Icons.access_alarm_outlined),
+                                  label: 'способности'),
+                              BottomNavigationBarItem(
+                                  icon: Icon(Icons.access_alarm_outlined),
+                                  label: 'инвентарь'),
+                              BottomNavigationBarItem(
+                                  icon: Icon(Icons.access_alarm_outlined),
+                                  label: 'личность'),
+                              BottomNavigationBarItem(
+                                  icon: Icon(Icons.access_alarm_outlined),
+                                  label: 'заклинания'),
+                            ],
+                          )
+                        : null,
                   ),
-                  body: const TabBarView(
-                    children: <Widget>[
-                      Fight(),
-                      Abilities(),
-                      Inventory(),
-                      Personality(),
-                      Spells(),
-                    ],
-                  ),
-                ),
-              );
-            }
+                );
+              },
+            );
           },
         );
       },
